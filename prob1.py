cc0c53129e1e751ad8ccba66a231ceb9d139b3d8
kieloh <nathan563412@gmail.com>
2024-02-13 20:14:59 -0600

    Finished prob1, 2, 3, and 4 and all are working great.

--- prob1.py
+++ prob1.py
@@ -1,4 +1,5 @@
+#!/usr/bin/env python3
 numbers = [1, 2, 3, 4]
 # Write a lambda function and use map
-squared_numbers = list(map(lambda x: _, numbers))
+squared_numbers = list(map(lambda x: x*x, numbers))
 print(squared_numbers)
